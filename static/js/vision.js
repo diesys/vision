b433e804406f02e9ddeeccf86019d6736b6f0a66
sbiego <forsesim@anche.no>
2017-08-25 23:35:37 +0200

    now really working change lang

--- static/js/vision.js
+++ static/js/vision.js
@@ -1,22 +1,19 @@
 function toggleLang() {
    var elementsIT = document.getElementsByClassName("desc_it");
    var elementsEN = document.getElementsByClassName("desc_en");
-   lan_sel = document.getElementById("lang_select");
+   var elementsLN = document.getElementsByClassName("lang_select");
    stateIT = elementsIT[0].style.display;
 
    for (var i = 0; i < elementsIT.length; i++) {
      if (stateIT == "none") {
+       elementsLN[i].className = "lang_select it";
        elementsIT[i].style.display = "block";
        elementsEN[i].style.display = "none";
      }
      else {
+       elementsLN[i].className = "lang_select en";
        elementsIT[i].style.display = "none";
        elementsEN[i].style.display = "block ";
      }
    }
-
-   if (stateIT == "none")
-     lan_sel.style.backgroundImage = 'url(../img/flaguk.png)';
-   else
-     lan_sel.style.backgroundImage = 'url(../img/flagit.png)';
 }
